f41a1f93fc9ffd1592b080be090270fae3b9c949
anthonysutardja <anthony.sutardja@gmail.com>
2015-04-19 14:22:18 -0700

    added time series data

--- stck/app.py
+++ stck/app.py
@@ -7,6 +7,7 @@ from flask import jsonify, render_template
 from feed.events import get_last_and_next_earnings_dates
 from feed.stocks import get_quote_from_google
 from feed.stocks import get_formatted_change
+from feed.stocks import get_time_series_from_yahoo
 
 
 app = Flask(__name__)
@@ -45,10 +46,21 @@ def get_general_stock_info_group(symbols):
 
 
 @app.route('/api/stock/<symbol>/general-info')
-def get_general_stock_info_individual(symbol):
-    data = get_general_info(symbol)
+def get_general_stock_info_individual(symbol, period):
+    data = get_time_series_from_yahoo(symbol, period)
 
+    # Supports only 1d, 5d, 90 day or 3 months, or 1 year.
     return jsonify(**data)
+
+
+@app.route('/api/stock/<symbol>/data/period/<period>')
+def get_symbol_data(symbol, period):
+    data = get_time_series_from_yahoo(symbol, period)
+    return jsonify(
+        symbol=symbol,
+        period=period,
+        data=data,
+    )
 
 
 if __name__ == '__main__':
--- stck/feed/stocks.py
+++ stck/feed/stocks.py
@@ -7,8 +7,13 @@
     Methods to fetch stock quotes from external sources.
 """
 import httplib
+import os
 import string
 import sys
+import time
+
+os.environ['TZ'] = 'US/Eastern'
+time.tzset()  # need to reset for eastern timezone
 
 
 def validate_stock_symbol(symbol):
@@ -107,6 +112,80 @@ def get_quote_from_yahoo(symbol):
     lastReading = response[-1].split(",")
     current, high, low = float(lastReading[1]), float(lastReading[2]), float(lastReading[3])
     return current, high, low
+
+
+def get_time_series_from_yahoo(symbol, period):
+
+    base_url = "chartapi.finance.yahoo.com"
+    query_path = "/instrument/1.0/{symbol}/chartdata;type=quote;range={period}/csv/"
+
+    if period not in ('1d', '5d', '30d', '1m', '3m', '6m', '1y'):
+        period = '1d'
+
+    symbol = validate_stock_symbol(symbol)
+
+    # Make request
+    conn = httplib.HTTPConnection(base_url)
+    conn.request("GET", query_path.format(symbol=symbol, period=period))
+    response = conn.getresponse().read().rsplit()
+    response = [r.split(',') for r in response if ':' not in r]
+    response = [r for r in response if len(r) == 6]
+
+    results = []
+    for r in response:
+        if period[-1] == 'd':
+            # use epoch time
+            t = time.localtime(float(r[0]))
+        else:
+            # use strptime
+            t = time.strptime(r[0], '%Y%m%d')
+        t = time.strftime('%Y-%m-%dT%H:%M:%SZ', t)
+        price = float(r[1])
+        results.append({'time': t, 'quote': price})
+    return results
+
+
+def get_time_series_from_google(symbol, period):
+    # NOTE DATA IS INCONSISTENT UPON MULTIPLE QUERIES..
+    base_url = "www.google.com"
+    QUERY_PATHS = {
+        '1d': '/finance/getprices?i=234&p=1d&f=d,c&df=cpct&q=',
+        '5d': '/finance/getprices?i=1170&p=5d&f=d,c&df=cpct&q=',
+        '1m': '/finance/getprices?i=7020&p=30d&f=d,c&df=cpct&q=',
+        '3m': '/finance/getprices?i=21060&p=90d&f=d,c&df=cpct&q=',
+        '1y': '/finance/getprices?i=60840&p=260d&f=d,c&df=cpct&q=',
+        'default': '/finance/getprices?i=234&p=1d&f=d,c&df=cpct&q=',
+    }
+
+    if period not in QUERY_PATHS:
+        period = 'default'
+
+    # Query must contain symbols in capital letters
+    symbol = validate_stock_symbol(symbol)
+
+    # Make request
+    conn = httplib.HTTPConnection(base_url)
+    conn.request("GET", QUERY_PATHS[str(period)] + symbol)
+    response = conn.getresponse().read().rsplit()
+
+    # Check if symbol was valid.
+    # strips away 'EXCHANGE%3D' from first line of response
+    checkQuote = str(response[0][11:])
+    if (checkQuote == 'UNKNOWN+EXCHANGE'):
+        raise ValueError("Symbol is not listed.")
+
+    # Return last quote
+    results = response[7:]
+    results = [tuple(r.split(',')) for r in results if 'TIMEZONE_OFFSET' not in r]
+
+    formatted_results = []
+    for date, price in results:
+        t = time.localtime(float(date[1:]))
+        t = time.strftime('%Y-%m-%dT%H:%M:%SZ', t)
+
+        res = {'time': t, 'quote': float(price)}
+        formatted_results.append(res)
+    return formatted_results
 
 
 if __name__ == "__main__":
